ac908744435e9c83f8d3e067011db53dc6ed7b84
wotjr1649 <wotjr980321@naver.com>
2024-06-22 20:03:39 +0900

    recommend-topic error edit

--- _recommend_topic_process.py
+++ _recommend_topic_process.py
@@ -14,10 +14,12 @@ async def recommend_data_preprocessing(data: dict) -> dict:
     )
     return {
         "_id": int(data.get("recommendation_id")),
-        "user_id": str(data.get("user_id")),
-        "user_mbti": int(data.get("user_mbti")),
-        "input_media_id": data.get("input_media_id").split(", "),
-        "recommended_media_id": data.get("recommended_media_id").split(", "),
+        "user_id": data.get("user_id"),
+        "user_mbti": data.get("user_mbti"),
+        "input_media_id": [int(media_id) for media_id in data.get("input_media_id").split(", ")],
+        "recommended_media_id": [
+            int(media_id) for media_id in data.get("recommended_media_id").split(", ")
+        ],
         "recommendation_time": pytz.timezone("Asia/Seoul").localize(
             datetime.strptime(data.get("recommendation_time"), "%Y-%m-%dT%H:%M:%S")
         ),
@@ -30,10 +32,8 @@ async def process_recommend_topic_message(mongo_client: AsyncIOMotorClient, mess
     print(f"type : {type(message)} and {message}")
 
     data = message["insert"]
-    table = data
+    table = "recommendation"
     data = data.get(table)
-
-    print(data)
     document = await recommend_data_preprocessing(data)
     await mongo_client["recommend"][table].insert_one(document)
     print(f"Inserted to MongoDB: {document}")
